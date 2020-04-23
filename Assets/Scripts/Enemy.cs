6d7b467d0b1384facefff2059d8f2295ade4eb1c
Joel De Labra <JoelDeLabra@gmail.com>
2020-04-23 15:42:13 -0700

    Enemigo parte 2, Navmesh y evadir obstáculos.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,25 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.AI;
+
 public class Enemy : MonoBehaviour
 {
-  [SerializeField, Range(0.1f,10f)]
-  float moveSpeed;
-  [SerializeField, Range(0.1f,10f)]
-  float minDistance;
+    [SerializeField, Range(0.1f, 10f)]
+    float moveSpeed;
+    [SerializeField, Range(0.1f, 10f)]
+    float minDistance;
 
-  void Update()
-  {
-    if(Attack)
+    NavMeshAgent navMeshAgent;
+
+    void Awake() 
     {
-      transform.Translate(transform.forward * moveSpeed * Time.deltaTime);
-      transform.LookAt(Gamemanager.instance.Player.transform);
-
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
-  }
 
-bool Attack
-  {
-      get => Vector3.Distance(this.transform.position, Gamemanager.instance.Player.transform.position) <= minDistance;
-  }
+
+    void Update() 
+    {
+        if(Attack)
+        {
+            //transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            navMeshAgent.destination = Gamemanager.instance.Player.transform.position;
+            transform.LookAt(Gamemanager.instance.Player.transform);
+        }
+    }
+
+    bool Attack
+    {
+        get => Vector3.Distance(this.transform.position, Gamemanager.instance.Player.transform.position) <= minDistance;
+    }
+
 }
